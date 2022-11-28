7b9d91545cc63dced21dfe656b92b27c055ddc78
khanhpham <khanhpham08121996@gmail.com>
2022-11-28 21:57:38 +0700

    update font

--- src/components/TermOfUsePopup/index.tsx
+++ src/components/TermOfUsePopup/index.tsx
@@ -13,33 +13,18 @@ export const TermOfUsePopup = () => {
     <div className="overlay" />
     <div className="modal">
       <div className="head">
-        <h1>Welcome</h1>
+        <h1 className='text-center'>BETA DISCLAIMER</h1>
       </div>
       <div className="content">
         <p>You are using the Beta/Demo/MVP version of Derivable Protocol.</p>
         <p>The Beta product might not function as intended, and some features are not available at the moment. Your fund
           might be lost due to bugs, hacks, and exploits.</p>
         <p>We would love to hear your experience with our product. Please report any bugs that you encountered to
-          https://t.me/derivablecs.</p>
+          <a href="https://t.me/derivablecs" target='_blank'> https://t.me/derivablecs.</a></p>
         <p>Our team greatly values your attention and experience.</p>
       </div>
       <div className="action">
         <div className='text-center accept-tou-wrap mb-2'>
-          {/*  <input*/}
-          {/*    type="checkbox"*/}
-          {/*    id='accept-tou'*/}
-          {/*    onChange={(e) => {*/}
-          {/*      setAgree(e.target.checked)*/}
-          {/*    }}*/}
-          {/*  />*/}
-          {/*  <label htmlFor="accept-tou" className='checkbox'>*/}
-          {/*    {agree ?*/}
-          {/*      <CheckedIcon />*/}
-          {/*      : <svg width="16" height="16" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">*/}
-          {/*        <rect width="11.7391" height="12" rx="2" fill="rgba(128, 128, 128, 0.4)"/>*/}
-          {/*      </svg>*/}
-          {/*    }*/}
-          {/*  </label>*/}
           <input
             type="checkbox"
             className='mr-05'
