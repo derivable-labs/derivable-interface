921d08ff2dfc5b7124ab6f73b2b7b3de3ea008d3
khanhpham <khanhpham08121996@gmail.com>
2022-11-29 16:25:38 +0700

    fix mobile menu

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -115,54 +115,22 @@ const Header = ({
           <img src={isPhone ? '/logo-white.png' : '/logo.png'} alt="" />
         }
       </Link>
-      {/*{*/}
-      {/*  isPhone ?*/}
-      {/*    <div className="navigation">*/}
-      {/*      <input type="checkbox" className="navigation__checkbox" id="navi-toggle" />*/}
 
-      {/*      { /* eslint-disable-next-line jsx-a11y/label-has-associated-control *!/*/}
-      {/*      <label htmlFor="navi-toggle" className="navigation__button">*/}
-      {/*        <span className="navigation__icon">&nbsp;</span>*/}
-      {/*      </label>*/}
+      {
+        !isPhone ?
+          <div className='menu'>
+            {
+              menus.map((menu, key) => {
+                return <Link
+                  to={menu.path}
+                  className={`menu--item ${(location.pathname.includes(menu.path) || (location.pathname === '/' && key === 0)) && 'active'}`}
+                >{menu.name}</Link>
+              })
+            }
+          </div>
+          : ''
+      }
 
-      {/*      <div className="navigation__background">&nbsp;</div>*/}
-
-      {/*      <nav className="navigation__nav">*/}
-      {/*        <ul className="navigation__list">*/}
-      {/*          {*/}
-      {/*            menus.map((menu) => {*/}
-      {/*              return <li className="navigation__item">*/}
-      {/*                <Link to={menu.path} className="navigation__link">*/}
-      {/*                  {menu.name}*/}
-      {/*                </Link>*/}
-      {/*              </li>*/}
-      {/*            })*/}
-      {/*          }*/}
-      {/*        </ul>*/}
-      {/*      </nav>*/}
-      {/*    </div>*/}
-      {/*    :*/}
-      {/*    <div className='menu'>*/}
-      {/*      {*/}
-      {/*        menus.map((menu, key) => {*/}
-      {/*          return <Link*/}
-      {/*            to={menu.path}*/}
-      {/*            className={`menu--item ${(location.pathname.includes(menu.path) || (location.pathname === '/' && key === 0)) && 'active'}`}*/}
-      {/*          >{menu.name}</Link>*/}
-      {/*        })*/}
-      {/*      }*/}
-      {/*    </div>*/}
-      {/*}*/}
-      <div className='menu'>
-        {
-          menus.map((menu, key) => {
-            return <Link
-              to={menu.path}
-              className={`menu--item ${(location.pathname.includes(menu.path) || (location.pathname === '/' && key === 0)) && 'active'}`}
-            >{menu.name}</Link>
-          })
-        }
-      </div>
       <div className='header__right'>
         <div className="hidden-on-phone network-select ">
           <Menu>
@@ -206,21 +174,60 @@ const Header = ({
           >Connect Wallet
           </div>
         }
-        <div className="header__right--bnt-setting">
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g clip-path="url(#clip0_496_468)">
-              <path
-                d="M5 10C3.9 10 3 10.9 3 12C3 13.1 3.9 14 5 14C6.1 14 7 13.1 7 12C7 10.9 6.1 10 5 10ZM19 10C17.9 10 17 10.9 17 12C17 13.1 17.9 14 19 14C20.1 14 21 13.1 21 12C21 10.9 20.1 10 19 10ZM12 10C10.9 10 10 10.9 10 12C10 13.1 10.9 14 12 14C13.1 14 14 13.1 14 12C14 10.9 13.1 10 12 10Z"
-                fill="white" />
-            </g>
-            <defs>
-              <clipPath id="clip0_496_468">
-                <rect width="24" height="24" fill="white" />
-              </clipPath>
-            </defs>
-          </svg>
-        </div>
+        {/*<div className="header__right--bnt-setting">*/}
+        {/*  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">*/}
+        {/*    <g clip-path="url(#clip0_496_468)">*/}
+        {/*      <path*/}
+        {/*        d="M5 10C3.9 10 3 10.9 3 12C3 13.1 3.9 14 5 14C6.1 14 7 13.1 7 12C7 10.9 6.1 10 5 10ZM19 10C17.9 10 17 10.9 17 12C17 13.1 17.9 14 19 14C20.1 14 21 13.1 21 12C21 10.9 20.1 10 19 10ZM12 10C10.9 10 10 10.9 10 12C10 13.1 10.9 14 12 14C13.1 14 14 13.1 14 12C14 10.9 13.1 10 12 10Z"*/}
+        {/*        fill="white" />*/}
+        {/*    </g>*/}
+        {/*    <defs>*/}
+        {/*      <clipPath id="clip0_496_468">*/}
+        {/*        <rect width="24" height="24" fill="white" />*/}
+        {/*      </clipPath>*/}
+        {/*    </defs>*/}
+        {/*  </svg>*/}
+        {/*</div>*/}
       </div>
+
+      {
+        isPhone ?
+          <div className="navigation">
+            <input type="checkbox" className="navigation__checkbox" id="navi-toggle" />
+
+            { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label htmlFor="navi-toggle" className="navigation__button">
+              <span className="navigation__icon">&nbsp;</span>
+            </label>
+
+            <div className="navigation__background">&nbsp;</div>
+
+            <nav className="navigation__nav">
+              <ul className="navigation__list">
+                {
+                  menus.map((menu) => {
+                    return <li className="navigation__item">
+                      <Link to={menu.path} className="navigation__link">
+                        {menu.name}
+                      </Link>
+                    </li>
+                  })
+                }
+              </ul>
+            </nav>
+          </div>
+          : ''
+      }
+      {/*<div className='menu'>*/}
+      {/*  {*/}
+      {/*    menus.map((menu, key) => {*/}
+      {/*      return <Link*/}
+      {/*        to={menu.path}*/}
+      {/*        className={`menu--item ${(location.pathname.includes(menu.path) || (location.pathname === '/' && key === 0)) && 'active'}`}*/}
+      {/*      >{menu.name}</Link>*/}
+      {/*    })*/}
+      {/*  }*/}
+      {/*</div>*/}
       <WalletModal
         visible={visibleWalletModal}
         setVisible={setVisibleWalletModal}
