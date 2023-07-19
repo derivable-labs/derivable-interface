1c9ec38252787426c100e21297580aa48491b53c
Zergity <zergity@gmail.com>
2023-07-19 16:34:49 +0700

    update Beta Discalimer text

--- src/components/TermOfUsePopup/index.tsx
+++ src/components/TermOfUsePopup/index.tsx
@@ -16,12 +16,12 @@ export const TermOfUsePopup = () => {
         <h1 className='text-center'>BETA DISCLAIMER</h1>
       </div>
       <div className="content">
-        <p>You are using the Beta/Demo/MVP version of Derivable Protocol.</p>
-        <p>The Beta product might not function as intended, and some features are not available at the moment. Your fund
-          might be lost due to bugs, hacks, and exploits.</p>
-        <p>We would love to hear your experience with our product. Please report any bugs that you encountered to
-          <a href="https://t.me/derivablecs" target='_blank'> https://t.me/derivablecs.</a></p>
-        <p>Our team greatly values your attention and experience.</p>
+        <p>You are using the Beta version of Derivable Protocol.</p>
+        <p>Before interacting with the current version, please be aware that the product is still in Beta and sometimes may not function properly. Some features may not be fully available on this version. The Beta product may also be prone to bugs, hacks, and exploits.</p>
+        <p>Since the Beta version only uses WETH-reserved pools, users do not need to approve any of their token to our contracts, so rest assured that there is no risk exposed to users' fund when interacting with our product.</p>
+        <p>Thank you for using Derivable Protocol in our earliest days. We would love to hear about your experience with our product. If you enjoy Derivable or encounter any bug along the way, please notify our team at
+          <a href="https://discord.gg/r7V97tSK" target='_blank'> https://discord.gg/r7V97tSK</a>.
+          The Derivable team greatly values your attention and experience!</p>
       </div>
       <div className="action">
         <div className='text-center accept-tou-wrap mb-2'>
