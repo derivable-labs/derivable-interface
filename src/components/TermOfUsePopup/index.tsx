715d479064da140514bc84c24ab4a380b4414b2c
Zergity <zergity@gmail.com>
2023-08-11 18:19:33 +0700

    update feedback discord permenant link

--- src/components/TermOfUsePopup/index.tsx
+++ src/components/TermOfUsePopup/index.tsx
@@ -19,7 +19,7 @@ export const TermOfUsePopup = () => {
         <p>You are using the Beta version of Derivable Protocol.</p>
         <p>Before interacting with the current version, please be aware that the product is still in Beta and sometimes may not function properly. Some features may not be fully available on this version. The Beta product may also be prone to bugs, hacks, and exploits.</p>
         <p>Since the Beta version only uses WETH-reserved pools, users do not need to approve any of their token to our contracts, so rest assured that there is no risk exposed to users' fund when interacting with our product.</p>
-        <p>Thank you for using Derivable Protocol in our earliest days. We would love to hear about your experience with our product. If you enjoy Derivable or encounter any bug along the way, please notify our team <a href="https://discord.gg/r7V97tSK" target='_blank'>here</a>. The Derivable team greatly values your attention and experience!
+        <p>Thank you for using Derivable Protocol in our earliest days. We would love to hear about your experience with our product. If you enjoy Derivable or encounter any bug along the way, please notify our team <a href="https://discord.gg/rjwDPyYMd4" target='_blank'>here</a>. The Derivable team greatly values your attention and experience!
         </p>
       </div>
       <div className="action">
